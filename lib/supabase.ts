590c4bdb3a9944c718ff4385c0710d129f126a3d
prakash jadhav <shopiwolf99@gmail.com>
2026-05-08 13:15:53 +0530

    Change date format from '-' to '/'

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -64,7 +64,7 @@ export const calculateNextServiceDate = (
   const y = date.getFullYear()
   const m = String(date.getMonth() + 1).padStart(2, '0')
   const d = String(date.getDate()).padStart(2, '0')
-  return `${y}-${m}-${d}` // manual format, no toISOString()
+  return `${y}/${m}/${d}` // manual format, no toISOString()
 }
 export type Customer = {
   id: string
